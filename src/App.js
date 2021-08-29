2992cc5f567ceda20fe0bc37a2a8888b286d5034
shoaib6174 <shoaib6174@gmail.com>
2021-08-30 02:00:24 +0600

    redirects

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import About from "./components/About";
 import Experience from "./components/Experience";
+import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 
@@ -133,6 +134,11 @@ class App extends Component {
           resumeExperience={this.state.resumeData.experience}
           resumeBasicInfo={this.state.resumeData.basic_info}
         />
+
+      {/* <Education
+          resumeExperience={this.state.resumeData.education}
+          resumeBasicInfo={this.state.resumeData.basic_info}
+        /> */}
         <Footer sharedBasicInfo={this.state.sharedData.basic_info} />
       </div>
     );
