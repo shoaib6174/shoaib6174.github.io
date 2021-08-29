974ea9ee0b86bb4cb2927cadfd68c415d2354d15
shoaib6174 <shoaib6174@gmail.com>
2021-08-30 00:42:48 +0600

    Updates

---
+++ precache-manifest.04cd1b6f7b7df15ab343a0832fcb96f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfdb1f010b5bb83a2b55ca5346dd1f8",
+    "url": "/Shoaib/index.html"
+  },
+  {
+    "revision": "c820c46f47c4b6b6898a",
+    "url": "/Shoaib/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "6aa657152507bdd4e7be",
+    "url": "/Shoaib/static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "c820c46f47c4b6b6898a",
+    "url": "/Shoaib/static/js/2.0a53f652.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/Shoaib/static/js/2.0a53f652.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa657152507bdd4e7be",
+    "url": "/Shoaib/static/js/main.3ed734d8.chunk.js"
+  },
+  {
+    "revision": "35d1fdea531548742af2",
+    "url": "/Shoaib/static/js/runtime-main.5c1a0107.js"
+  }
+]);
